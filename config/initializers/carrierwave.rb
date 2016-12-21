c3311b333ef26d6cea8c0fb38c893fc13e214a53
Alexander Litvinchuk <alex.litvinchuk@molinos.ru>
2016-12-21 15:50:28 +0200

    config for aws storage hotfix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
 	config.fog_credentials = {
 			# Configuration for Amazon S3
 			:provider              => 'AWS',
-			:aws_access_key_id     => ENV['S3_KEY'],
-			:aws_secret_access_key => ENV['S3_SECRET'],
-			:region                => ENV['S3_REGION']
+			:aws_access_key_id     => '',
+			:aws_secret_access_key => '',
+			:region                => ''
 	}
 
 	# For testing, upload files to local `tmp` folder.
@@ -23,7 +23,7 @@ CarrierWave.configure do |config|
 
 	config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
-	config.fog_directory    = ENV['S3_BUCKET_NAME']
+	config.fog_directory    = ''
 	config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-	config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+	config.fog_host         = ''
 end
