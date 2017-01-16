fce30e5a596e8968830af8b93be260e499299b17
Alexander Litvinchuk <alex.litvinchuk@molinos.ru>
2017-01-16 02:12:17 +0200

    votes statistics

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,6 @@ class VotesController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@votes = LikesPhoto.all
+		@votes = LikesPhoto.where(user_id: current_user.id)
 	end
 end
